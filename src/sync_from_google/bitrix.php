ca65dfbf77192c31555e8b9a1846274a33bde2c1
Alexander Kolos <52374795+als404@users.noreply.github.com>
2025-10-09 11:02:05 +0300

    Update bitrix.php
    добавил возможность запуска скрипта из sheel

--- src/sync_from_google/bitrix.php
+++ src/sync_from_google/bitrix.php
@@ -1,4 +1,24 @@
 <?php
+$_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/ext_www/public_html";
+$DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
+
+$siteID = 's1';
+//$siteID = '#SITE_ID#';  // your site ID - need for language ID
+
+define("NO_KEEP_STATISTIC", true);
+define("NOT_CHECK_PERMISSIONS",true);
+define("BX_CAT_CRON", true);
+define('NO_AGENT_CHECK', true);
+if (preg_match('/^[a-z0-9_]{2}$/i', $siteID) === 1)
+{
+	define('SITE_ID', $siteID);
+}
+else
+{
+	die('No defined site - $siteID');
+}
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include.php');
 
     // Проверяем, что include.php подключён
